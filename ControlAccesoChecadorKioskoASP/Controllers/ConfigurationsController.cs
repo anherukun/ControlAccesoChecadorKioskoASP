95287c7bffe069a47bcb42810ceaeb55a651c2e3
Angel Jimenez <angel.g.j.reyes@outlook.com>
2020-06-24 22:09:18 -0500

    + Creacion del proyecto + Controller para Configuraciones + Repository para Departmentos + Vista para Configurations/Index + Vista para Home/Index (Panel de registro)

--- ControlAccesoChecadorKioskoASP/Controllers/ConfigurationsController.cs
+++ ControlAccesoChecadorKioskoASP/Controllers/ConfigurationsController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ControlAccesoChecadorKioskoASP.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,6 +12,7 @@ namespace ControlAccesoChecadorKioskoASP.Controllers
         // GET: Configurations
         public ActionResult Index()
         {
+            ViewData["Departments"] = new DepartmentRepository().RetriveAll();
             return View();
         }
     }
---
+++ ControlAccesoChecadorKioskoASP/Services/DepartmentRepository.cs
@@ -0,0 +1,19 @@
+﻿using ControlAccesoChecadorKioskoASP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ControlAccesoChecadorKioskoASP.Services
+{
+    public class DepartmentRepository
+    {
+        public List<Department> RetriveAll()
+        {
+            using (var db = new ControlAccessCheckerContext())
+            {
+                return db.Departments.ToList();
+            }
+        }
+    }
+}
