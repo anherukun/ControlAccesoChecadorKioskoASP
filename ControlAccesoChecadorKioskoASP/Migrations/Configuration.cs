ce50c69731cad157df4399b81d302fa81e1aadfb
Angel Jimenez <angel.g.j.reyes@outlook.com>
2020-07-06 08:34:33 -0500

    + Bugfix: Al seleccionar un departamento que no tenia registros creados intentaba rendeerizar elementos pero la lista era null. Asi que ocurria un OutIndexException

--- ControlAccesoChecadorKioskoASP/Migrations/Configuration.cs
+++ ControlAccesoChecadorKioskoASP/Migrations/Configuration.cs
@@ -10,7 +10,6 @@
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
-
         }
 
         protected override void Seed(ControlAccesoChecadorKioskoASP.Models.ControlAccessCheckerContext context)
