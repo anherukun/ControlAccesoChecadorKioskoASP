463ab21ae2277a81efa29803309f098da9ead3ac
Angel Jimenez <angel.g.j.reyes@outlook.com>
2020-07-19 20:27:02 -0500

    +

--- ControlAccesoChecadorKioskoASP/Controllers/AdministrationController.cs
+++ ControlAccesoChecadorKioskoASP/Controllers/AdministrationController.cs
@@ -2,6 +2,7 @@
 using ControlAccesoChecadorKioskoASP.Services;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -19,13 +20,40 @@ namespace ControlAccesoChecadorKioskoASP.Controllers
         public ActionResult ReportAccess()
         {
             List<Department> departments = new DepartmentRepository().RetriveAll();
+            List<Employe> employes = new EmployeRepsitory().RetriveAll();
+
+            ViewData["Departments"] = departments;
+            ViewData["Employes"] = employes;
 
             return View();
         }
 
         [HttpPost]
-        public ActionResult SubmitReportAccess(string dateStart, string dateEnd, int department = 0, int employeId = 0)
+        public ActionResult ReportAccess(string dateStart, string dateEnd, int department = 0, int employeId = 0)
         {
+            List<Department> departments = new DepartmentRepository().RetriveAll();
+            List<Employe> employes = new EmployeRepsitory().RetriveAll();
+            List<AccessRegistry> accessRegistries;
+
+            ViewData["Departments"] = departments;
+            ViewData["Employes"] = employes;
+
+            if (department != 0)
+            {
+                accessRegistries = new AccessRegistryRepository().RetriveByDepartment(department, DateTime.Parse(dateStart), DateTime.Parse(dateEnd));
+                ViewData["AccessRegistries"] = accessRegistries;
+            }
+            else if (employeId != 0)
+            {
+                accessRegistries = new AccessRegistryRepository().RetriveByEmploye(employeId, DateTime.Parse(dateStart), DateTime.Parse(dateEnd));
+                ViewData["AccessRegistries"] = accessRegistries;
+            }
+            else
+            {
+                accessRegistries = new AccessRegistryRepository().RetriveAll(DateTime.Parse(dateStart), DateTime.Parse(dateEnd));
+                ViewData["AccessRegistries"] = accessRegistries;
+            }
+
             return View();
         }
     }
